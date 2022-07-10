401c97a8687fcb6c06444b609bec5e06ddaba96f
starplanter93 <starplanter93@gmail.com>
2022-07-10 17:55:25 +0900

    [BOJ] 두 수 비교하기 / 브론즈5 / 10분

---
+++ week1/조건문/b1330/b1330.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().split(" ");
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+
+if( A > B ){
+    console.log(">");
+}else if ( A < B ){
+    console.log("<")
+}else if( A === B ){
+    console.log("==");
+}
