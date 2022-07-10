a171603bd2879c81743b89296165c940942761cc
chocholove <93895421+devminoh@users.noreply.github.com>
2022-07-10 17:45:29 +0900

    Delete gyeongmin/week2 directory

--- gyeongmin/week2/2739.js
+++
@@ -1,8 +0,0 @@
-let fs = require('fs');
-let input = fs.readFileSync('/dev/stdin').toString().split(' ');
-
-let num = Number(input);
-
-for(let i = 1; i < 10; i++){
-    console.log(`${num} * ${i} = ${num*i}`)
-}
