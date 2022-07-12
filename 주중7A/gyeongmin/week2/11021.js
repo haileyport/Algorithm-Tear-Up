92917f7311e18e673bfbe1b4a6778cd6b625dc11
chocholove <93895421+devminoh@users.noreply.github.com>
2022-07-12 19:40:48 +0900

    [BOJ]11021/bronze/1min

---
+++ 주중7A/gyeongmin/week2/11021.js
@@ -0,0 +1,7 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+for (let i = 1; i <= input[0]; i++) {
+    let numbers = input[i].split(' ');
+    
+    console.log(`Case #${i}: ${Number(numbers[0]) + Number(numbers[1])}`);
+}
