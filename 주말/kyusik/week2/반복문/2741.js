d10e24e13b5965db1fb2d7ea5aa04e31a3b3e6f6
starplanter93 <starplanter93@gmail.com>
2022-07-12 19:49:50 +0900

    [BOJ] N 찍기 / 브론즈5 / 30분

--- 주말/kyusik/week2/반복문/2741.js
+++ 주말/kyusik/week2/반복문/2741.js
@@ -3,8 +3,10 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 let input = fs.readFileSync(filePath).toString().trim().split();
 
 let n = parseInt(input[0]);
-let result = 0;
+let result = '';
+
 for (let i = 1; i <= n; i++) {
-    result = i;
-    console.log(result);
+    result += i + '\n';
 }
+
+console.log(result);
