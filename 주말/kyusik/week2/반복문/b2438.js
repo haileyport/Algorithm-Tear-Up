41bd7d6ebb34bc8c8f71919b5e2b64d57d1765f7
starplanter93 <starplanter93@gmail.com>
2022-07-14 19:10:21 +0900

    [BOJ] 별 찍기 - 1 / 브론즈 5 / 30분

--- 주말/kyusik/week2/반복문/b2438.js
+++ 주말/kyusik/week2/반복문/b2438.js
@@ -9,8 +9,8 @@ let n = input[0];
 let result = '';
 
 for (let i = 1; i <= n; i++) {
-    console.log(result+'*');
-    result = result + '*';
+    console.log(result+'*'); // 우선 ''+'*'로 * 출력
+    result = result + '*'; //그 뒤로 for문이 돌때마다 '*'이 이전 결과에 더해져서 출력
 }
 
 //console.log(result);
