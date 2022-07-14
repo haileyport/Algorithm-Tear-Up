481dd18ccfd93c60199d194f8e8a9cc20c944533
tmdn01cs <tmdn01cs@gmail.com>
2022-07-14 11:24:38 +0900

    [BOJ] 최댓값

--- 주중7B/seongyoon/week3/10818.js
+++ 주중7B/seongyoon/week3/10818.js
@@ -2,4 +2,4 @@ let input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n
 let N = +input[0];
 let nums = input[1].split(' ').map(Number);
 
-console.log(`${Math.min.apply(Math, nums)} ${Math.max.apply(Math, nums)}`)
+console.log(`${Math.min(...nums)} ${Math.max(...nums)}`);
