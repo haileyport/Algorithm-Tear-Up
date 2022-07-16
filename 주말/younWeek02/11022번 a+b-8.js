fddf92548a30dcbda1b794b752fc50c0c1bd0f8f
skyhanull <anky0425@naver.com>
2022-07-16 16:37:54 +0900

    [BOJ] a+b-8/ 브론즈5 / 3분

--- 주말/younWeek02/11022번 a+b-8.js
+++ 주말/younWeek02/11022번 a+b-8.js
@@ -7,5 +7,5 @@ for(let i = 1 ; i <= num ; i ++){
     let result = input[i].split(' ');
     let a = Number(result[0]);
     let b = Number(result[1]);
-      console.log(`Case #${i}: ${a} + ${b} = ${a+b}`);
+      console.log(`Case #${i}: ${a} + ${b} = ${a+b}`);//
 }
