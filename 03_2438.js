ddf8be84ce368a177b6fe7c6b393f71e13551dab
Jeong Ha Seung <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-07-16 16:23:28 +0900

    Delete 03_2438.js

--- 03_2438.js
+++
@@ -1,9 +0,0 @@
-const fs = require("fs");
-const testCase = fs.readFileSync(0, "utf-8").toString();
-
-let space = "";
-
-for (let i = 0; i < testCase; i++) {
-  space += "*";
-  console.log(space);
-}
