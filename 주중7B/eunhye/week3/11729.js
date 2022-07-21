7e1e4c6a39d961abef7dc28f0edad4dd88d84e5e
haileyport <sugar9935@gmail.com>
2022-07-21 17:01:57 +0900

    [BOJ] 하노이 탑 이동 순서 / 실버 1 / 40분

--- 주중7B/eunhye/week3/11729.js
+++ 주중7B/eunhye/week3/11729.js
@@ -2,24 +2,29 @@
 
 const fs = require("fs");
 const input = fs.readFileSync("./text.txt").toString();
-//input - 원판 수
-//하노이 공식 An = 2^n - 1번으로 이동 가능
-//이동 루트??
-//홀수일 때는 3으로, 짝수일때는 2로 보내서 시작
-//to3, to2 함수를 번갈아가면서 사용하면 될 것 같은데
 
-const moveCount = (pan) => {
-  return 2 ** pan - 1;
-};
+let N = Number(input); // 원판의 갯수
+let count = 0;
+let answer = [];
 
-const toThree = () => {};
+// num : 원반의 개수
+// from : 출발지 기둥 번호
+// to : 목적지 기둥 번호
+// other : 나머지 기둥 번호
 
-const toTwo = () => {};
-
-const moving = (pan) => {
-  if (pan === 1) return 1;
-  pan % 2 === 0 ? toTwo() : toThree();
-};
-
-const ans = moveCount(Number(input));
-console.log(ans);
+function Hanoi(num, from, other, to) {
+  if (num === 0) {
+    return;
+  } else {
+    // 받아온 원반 갯수보다 하나 적은 원반들을 목적지가 아닌 곳으로 재귀적으로 이동
+    Hanoi(num - 1, from, to, other);
+    // 맨 아래 원반을 목적지로 이동시킴
+    answer.push([from, to]);
+    count++;
+    //다른 곳으로 옮겼던 원반들을 그 위에 얹음
+    Hanoi(num - 1, other, from, to);
+  }
+}
+Hanoi(N, "1", "2", "3");
+console.log(count);
+console.log(answer.map((element) => element.join(" ")).join("\n"));
