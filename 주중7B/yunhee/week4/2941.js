131535ae851eabddcc34c325261a9fb220592877
KimYunhee <qnvnfls@naver.com>
2022-07-26 08:34:21 +0900

    [BOJ] 크로아티아 알파벳 / 실버5 / 1시간(실패)

--- 주중7B/yunhee/week4/2941.js
+++ 주중7B/yunhee/week4/2941.js
@@ -4,13 +4,13 @@ let fs = require("fs");
 let input = fs.readFileSync("input.txt").toString().trim();
 
 function solution(input){
-    
     let arr = ["c=", "c-", "dz=", "d-",  "lj", "nj", "s=", "z="];
     let plusLeng = 0;
     let minusLeng = 0;
 
     for(let i = 0; i < arr.length; i++){
         let str = input;
+        
         if(str.includes(arr[i])){
             str = str.replaceAll(arr[i], "#")
             let count = 0;
