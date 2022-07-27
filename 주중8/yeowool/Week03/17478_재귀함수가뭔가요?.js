a6dc4ba7453a7ac5836bf3f2f91c5a728050ca5b
yeowool <qyoong3579@gmail.com>
2022-07-27 13:05:33 +0900

    [BOJ] 17478 재귀함수가뭔가요? / 실버5(수정)

--- 주중8/yeowool/Week03/17478_재귀함수가뭔가요?.js
+++ 주중8/yeowool/Week03/17478_재귀함수가뭔가요?.js
@@ -39,3 +39,28 @@ console.log(
 //이렇게 나왔다.
 
 // 한꺼번에 나왔다. 이걸 한번씩 출력하게 하려면 어떻게 할까..?
+
+// 한 문장씩 끊어야하나..?
+
+let line1 = `어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.`
+let line2 = `"재귀함수가 뭔가요?"`
+let line3 = `"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어."`
+let line4 = `라고 답변하였지.`
+let underbar = `____`
+
+function answer(input) {
+    let result
+    for(let i = 0; i < input; i++) {
+        underbar += underbar
+        for(let j = 0; j < input; j++) {
+            result = line2 + "\n" + line3
+        }
+        console.log(underbar)
+        console.log(result)
+    }
+}
+// 생각하기를 포기..ㅜㅜ 나는 왜 머리가 안돌아가는가
+
+// 짜게 식었다.. 훌륭한 성훈님의 풀이를 보며 이 문제는 마무리!!
+
+
