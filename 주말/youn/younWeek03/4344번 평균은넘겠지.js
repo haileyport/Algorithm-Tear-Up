5ddb36ef43940203c93bf29ca78732291fff07e7
skyhanull <anky0425@naver.com>
2022-07-30 14:14:24 +0900

    [BOJ] 아스키코드 / 브론즈5 / 1분

--- 주말/youn/younWeek03/4344번 평균은넘겠지.js
+++ 주말/youn/younWeek03/4344번 평균은넘겠지.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
 let num = Number(input[0]);
 
