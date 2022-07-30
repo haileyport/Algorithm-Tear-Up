214e39f46ee981bf4525bb0951cea8ba5a78fbbe
skyhanull <anky0425@naver.com>
2022-07-30 14:08:08 +0900

    주말/4주차

---
+++ 주말/youn/younWeek04/10809.js
@@ -0,0 +1,9 @@
+const input = require("fs").readFileSync("/dev/stdin").toString();
+
+const result = [];
+
+for (let i = 97; i <= 122; i++) {
+  result.push(input.indexOf(String.fromCharCode(i)));
+}
+
+console.log(result.join(" "));
---
+++ 주말/youn/younWeek04/ASCII code.js
@@ -0,0 +1,7 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+let str = input.toString();
+result = str.charCodeAt(0);
+
+console.log(result);
