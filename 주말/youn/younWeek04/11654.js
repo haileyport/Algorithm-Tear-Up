22fed634bcec147812c4551d6ba7f0b7380352f2
skyhanull <anky0425@naver.com>
2022-07-30 14:24:22 +0900

    [BOJ]아스키코드 / 브론즈5 / 2분

--- 주말/youn/younWeek04/11654.js
+++ 주말/youn/younWeek04/11654.js
@@ -2,6 +2,6 @@ let fs = require('fs');
 let input = fs.readFileSync('/dev/stdin').toString().split(' ');
 
 let str = input.toString(); 
-result = str.charCodeAt(0);
+result = str.charCodeAt(0); //
 
 console.log(result);
