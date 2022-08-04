f0d739fef8d68a87e2e261e388ca9e1df48e00ae
haileyport <sugar9935@gmail.com>
2022-08-04 14:16:40 +0900

    [BOJ] 베르트랑 공준 / 실버3 / 22분

--- 주중7B/seongyoon/week3/3052.js
+++
@@ -1,9 +0,0 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-
-let arr = [];
-for (i=0; i<10; i++) {
-    if (!(arr.includes(Number(input[i])%42))) {
-        arr.push(Number(input[i])%42);
-    }
-}
-console.log(arr.length);
--- 주중7B/seongyoon/week4/2941.js
+++
@@ -1,8 +0,0 @@
-let input = require('fs').readFileSync('/dev/stdin').toString().trim()
-
-let croatia = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="];
-
-for (let el of croatia) {
-    input = input.split(el).join('*');
-}
-console.log(input.length);
