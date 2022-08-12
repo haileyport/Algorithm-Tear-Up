60575262dcecf7c86478b64551e357cb01c6d90d
Choizia <107836206+choizia0724@users.noreply.github.com>
2022-08-12 12:49:28 +0900

    Delete 주중7A/kimjiyeon directory

--- 주중7A/kimjiyeon/week2/2439.js
+++
@@ -1,7 +0,0 @@
-let fs = require('fs');
-let input = fs.readFileSync('dev/stdin').toString();
-
-let count = Number(input);
-for(let i = 1; i<=count;i++){
-    console.log(" ".repeat(count-i)+"*".repeat(i));
-}
