69e9f1ef7b0296ad1a0e53bca4937126ae5471e6
Choizia <107836206+choizia0724@users.noreply.github.com>
2022-08-12 12:43:23 +0900

    Delete 주중7A/Kimjiyeon/week6 directory

--- 주중7A/Kimjiyeon/week6/25305.js
+++
@@ -1,9 +0,0 @@
-let fs = require('fs');
-let input = fs.readFileSync("/dev/stdin").toString().split("\n")
-input.map(x=>x.split(' '))
-let count = input.shift() 
-count = count.split(' ');
-let cutline = Number(count[1])
-input = input[0].split(' ')
-input.sort((a,b)=>b-a)
-console.log(input[cutline-1])
