b104cf4aae9516bd441b85b93ea74b81156774ae
headring <myhakiki@gmail.com>
2022-08-19 01:48:40 +0900

    [PGS]하샤드수/LV1/풀이시간5분/3차수정안

--- 주말/sanghyun/Week7(Programmers)/5.하샤드수.js
+++ 주말/sanghyun/Week7(Programmers)/5.하샤드수.js
@@ -1,17 +1,31 @@
 function solution(x) {
-  let sum = String(x).split('').map(el => Number(el)).reduce((a, b) => a + b)
-  // console.log(sum);
-  return x % sum === 0 ? true : false;
+  // 1차
+  // var answer = true;
+  // let input = String(x);
+  // let sum = 0; 
+  // for(let i of input){
+  //     sum += Number(i);    
+  // }
+  // if(x%sum == 0) {
+  //     return answer;
+  // }else{
+  //     return false;
+  // }
+  // 2차
   // let divider = 0;
   // let numString = String(x);
   // for (let i = 0; i < numString.length; i++){
-  //   console.log(numString[i])
-  //   divider += Number(numString[i])
+  //     console.log(numString[i])
+  //     divider += Number(numString[i])
   // }
   // console.log(divider)
   // if(x % divider === 0){
-  //   return true;
+  //     return true;
   // }else{
-  //   return false;
+  //     return false;
   // }
+  // 3차
+  let sum = String(x).split('').map(el => Number(el)).reduce((a,b) => a + b)
+  // console.log(sum);
+  return x % sum === 0 ? true : false;
 }
