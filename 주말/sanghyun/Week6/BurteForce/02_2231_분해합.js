a4d8f8457f5dda971d151e2c36a781ae94254c87
headring <myhakiki@gmail.com>
2022-08-20 11:32:48 +0900

    분해합(풀이시60분)주석 수정

--- 주말/sanghyun/Week6/BurteForce/02_2231_분해합.js
+++ 주말/sanghyun/Week6/BurteForce/02_2231_분해합.js
@@ -6,23 +6,25 @@ const solution = (income) => {
   let targetNum = Number(income)
   let arr = [];
 
-  // 10미만은 그냥 그 자체를 출력
+  
   for (let i = 0; i <= 1000000; i++) {
-    let stringNum = String(i);
-    let result = i;
+    let stringNum = String(i); //'198'
+    let result = i; // 198
     for (let j = 0; j < stringNum.length; j++) {
-      result += Number(stringNum[j])
+      result += Number(stringNum[j]) // 198 + 1 + 9 + 8
+      // 분해합이 완성되었을 경우를 판별
       if (j === (stringNum.length - 1) && result === targetNum) {
         arr.push(i);
       }
     }
   }
 
-  // console.log(arr);
+  console.log(arr);
   if (arr.length !== 0) {
     console.log(arr[0]);
   } else {
     console.log(0);
   }
 }
+
 solution(input)
