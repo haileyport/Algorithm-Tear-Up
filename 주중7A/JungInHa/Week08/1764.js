ff3726182b75f9a6b4369632044eb1caabb1fa57
JungInHa <juudyy99@gmail.com>
2022-08-26 00:24:17 +0900

    [BOJ] 듣보잡 / 실버4 / 리팩토링

--- 주중7A/JungInHa/Week08/1764.js
+++ 주중7A/JungInHa/Week08/1764.js
@@ -5,15 +5,11 @@ const input = fs.readFileSync('./input.txt').toString().trim().split("\n");
 let result = [];
 
 const unheardNum = Number(input[0].split(' ')[0]);
-const unseenNum = Number(input[0].split(' ')[1]);
 const unheard = new Set(input.slice(1, unheardNum + 1));
 const unseen = new Set(input.slice(unheardNum + 1));
 
-const longer = (unheardNum >= unseenNum) ? unheard : unseen;
-const shorter = (unheardNum < unseenNum) ? unheard : unseen;
-
-longer.forEach(el => {
-  if (shorter.has(el)) result.push(el);
+unheard.forEach(el => {
+  if (unseen.has(el)) result.push(el);
 })
 
 result.sort();
