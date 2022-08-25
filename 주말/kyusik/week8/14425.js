a447afae515f0b8c01043984b911eb7613e707d5
starplanter93 <starplanter93@gmail.com>
2022-08-25 20:45:40 +0900

    [BOJ]문자열 집합 / 실버3 / 80분

--- 주말/kyusik/week8/14425.js
+++ 주말/kyusik/week8/14425.js
@@ -4,51 +4,79 @@ let input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const MS = input[0].split(' ');
 const M = parseInt(MS[0]);
-//const S = parseInt(MS[1]);
 const MStrings = input.slice(1, 1 + M);
 const SStrings = input.slice(1 + M);
 let count = 0;
+// MStrings엔 중복단어 가능, SStrings엔 중복단어 불가.
 
-// for(const el of SStrings){
-//     if(MStrings.includes(el)){
-//         count ++;
-//     }  
-// }
-
-// console.log(count);
-//MString에 중복되는 문자열이 많아지면 시간초과가 나는것 같다.
-// set을 쓰면 중복되는 문자열을 없애주기때문에 set을써보자
-
+// case 1-1 SStrings의 요소를 Mset에 넣어 비교 - 정답
 const MSet = new Set(MStrings);
 SStrings.forEach((el) =>{
     if(MSet.has(el)) count++;
 })
 console.log(count);
 
-/* for(const el of MStrings){
-    if(SStrings.includes(el)){
-        count ++;
-   //문제에서 S에는 중복이 없다고했지만 M에는 그런 조건이 없었다. 만약 M에 중복 단어가 있다면 이 코드로는 중복된 갯수만큼 카운트가 더 되서 오답임.
-    }
-}
-    ex)    const MStrings = input.slice(1, 1 + M);
-    [
-        'baekjoononlinejudge',
-        'startlink',
-        'startlink',
-        'sundaycoding',
-        'codingsh'
-    ] 
-    const SStrings = input.slice(1 + M);
-    [
-        'baekjoon',     'codeplus',
-        'codeminus',    'startlink',
-        'starlink',     'sundaycoding',
-        'codingsh',     'codinghs',
-        'sondaycoding', 'startrink',
-        'icerink'
-    ] */
+// case 1-2 Mset의 요소를 SStrings에 넣어 비교 - 오답
+// 그런데 case 1-1과 값은 같게 나옴. MStrings에 중복 단어가 있어도 set때문에 중복이 사라지기 때문에 당연함.
+// const MSet = new Set(MStrings);
+// MSet.forEach((el) =>{
+//     if(SStrings.includes(el)) count++;
+// })
+// console.log(count);
 
+// case 2-1 set안쓰고 배열끼리 비교, SStrings의 요소를 MStrings에 넣어 비교 - 시간초과
+// for(const el of SStrings){
+//     if(MStrings.includes(el)){
+//         count ++;
+//     }  
+// }
+// console.log(count);
+
+// case 2-2 set안쓰고 배열끼리 비교, MStrings의 요소를 SStrings에 넣어 비교 - 오답
+// for(const el of MStrings){
+//     if(SStrings.includes(el)){
+//         count ++;
+//     }
+// }
+// console.log(count);
+
+/* 백준 입력
+5 11
+baekjoononlinejudge
+startlink
+codeplus
+sundaycoding
+codingsh
+baekjoon
+codeplus
+codeminus
+startlink
+starlink
+sundaycoding
+codingsh
+codinghs
+sondaycoding
+startrink
+icerink */
+
+/* //백준입력에서 M에 문자 하나를 중복문자로 교체한 테스트케이스
+5 11
+baekjoononlinejudge
+startlink
+startlink
+sundaycoding
+codingsh
+baekjoon
+codeplus
+codeminus
+startlink
+starlink
+sundaycoding
+codingsh
+codinghs
+sondaycoding
+startrink
+icerink */
 
 
 'https://stackoverflow.com/questions/39007637/javascript-set-vs-array-performance'
