6188371aa639127230df84698a3b180f69f144d2
headring <myhakiki@gmail.com>
2022-08-27 19:26:08 +0900

    [BOJ]집합-문자열집합/실버3/풀이시간 60분

---
+++ 주말/sanghyun/Week8/Set_Map/02_14425_문자열집합.js
@@ -0,0 +1,30 @@
+let fs = require('fs');
+// let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+let input = fs.readFileSync('./02_14425.txt').toString().trim().split('\n');
+
+function solution(income){
+  let dicIndex = income[0].split(' ').map(Number)[0];
+  let dictionary = new Set(income.slice(1, dicIndex + 1))
+  let toLook = (income.slice(dicIndex + 1));
+  // console.log(dictionary);
+  // console.log(toLook);
+  console.log(toLook.filter(el => dictionary.has(el)).length);
+
+}
+solution(input)
+
+/*
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+function solution(income){
+  let dicIndex = income[0].split(' ').map(Number)[0];
+  let dictionary = new Set(income.slice(1, dicIndex + 1))
+  let toLook = new Set(income.slice(dicIndex + 1));
+  // console.log(dictionary);
+  // console.log(toLook);
+  console.log([...toLook].filter(el => dictionary.has(el)).length);
+
+}
+solution(input)
+*/
