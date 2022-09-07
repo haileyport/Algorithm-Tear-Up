dd2acbbc63e4d6ddd8538f106909641745009a50
JungInHa <juudyy99@gmail.com>
2022-09-07 16:32:00 +0900

    [PGS] 최대공약수와 최소공배수 / Level 1 / 리팩토링

--- 주중7A/JungInHa/Week10/최대공약수와최소공배수.js
+++ 주중7A/JungInHa/Week10/최대공약수와최소공배수.js
@@ -1,23 +1,16 @@
 function solution(n, m) {
-  const min = Math.min(n, m);
-  const max = Math.max(n, m);
-  
-  let commonFactor; 
-  for (let i = min; i > 0; i--) { 
-      if (n % i === 0 && m % i === 0) {
-          commonFactor = i;
-          break;
-      }
+    const min = Math.min(n, m);
+    
+    let commonFactor; 
+    for (let i = min; i > 0; i--) { 
+        if (n % i === 0 && m % i === 0) {
+            commonFactor = i;
+            break;
+        }
+    }
+    
+    let commonMultiple = n * m / commonFactor;
+    
+    let answer = [commonFactor, commonMultiple];
+    return answer;
   }
-  
-  let commonMultiple; 
-  for (let i = max; i > 0; i++) {
-      if (i % n === 0 && i % m === 0) {
-          commonMultiple = i;
-          break;
-      }
-  }
-  
-  let answer = [commonFactor, commonMultiple];
-  return answer;
-}
