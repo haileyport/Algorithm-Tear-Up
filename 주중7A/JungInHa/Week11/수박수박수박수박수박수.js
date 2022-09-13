8f6a5a8015e84693419a7c639cf6b59ceab76a95
JungInHa <juudyy99@gmail.com>
2022-09-13 19:55:31 +0900

    [PGS] 수박수박수박수박수박수? / Level 1 / 리팩토링

--- 주중7A/JungInHa/Week11/수박수박수박수박수박수.js
+++ 주중7A/JungInHa/Week11/수박수박수박수박수박수.js
@@ -1,9 +1,3 @@
 function solution(n) {
-  let answer = '';
-
-  for (let i = 0; i < n; i++) {
-      answer += (i % 2 === 0) ? '수' : '박';
-  } 
-
-  return answer;
+  return '수박'.repeat(n/2) + (n%2 === 1 ? '수' : '');
 }
