0fa9a76586ff2cf89594e0f1fe3cb6d9480a96fd
JungInHa <juudyy99@gmail.com>
2022-09-13 19:55:39 +0900

    [PGS] 소수 찾기 / Level 1 / 리팩토링

--- 주중7A/JungInHa/Week11/소수찾기.js
+++ 주중7A/JungInHa/Week11/소수찾기.js
@@ -1,17 +1,17 @@
+function checkPrime(n) {
+    for (let j = 2; j <= Math.sqrt(n); j++) {
+        if (n % j === 0) {
+            return false;
+        }
+    } 
+    return true;
+}
+
 function solution(n) {
-  let answer = 0;
-  
-  for (let i = 2; i <= n; i++) {
-      let isPrime = true;
-      
-      for (let j = 2; j <= Math.sqrt(i); j++) {
-          if (i % j === 0) {
-              isPrime = false;
-              break;
-          }
-      }
-      
-      if (isPrime) answer++;
-  }
-  return answer;
+    let answer = 0;
+    
+    for (let i = 2; i <= n; i++) {      
+        if (checkPrime(i)) answer++;
+    }
+    return answer;
 }
