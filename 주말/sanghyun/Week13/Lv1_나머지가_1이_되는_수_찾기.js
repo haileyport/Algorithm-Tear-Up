8015036cb69ef58d42148825fdfde0efed69aa78
headring <myhakiki@gmail.com>
2022-10-13 00:52:53 +0900

    [PGS]나머지가 1이 되는 수 찾기/Lv1/풀이시간 15분

---
+++ 주말/sanghyun/Week13/Lv1_나머지가_1이_되는_수_찾기.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  let one = n-1;
+  if(n % one == 1){
+      for (let a = 2 ; a <= one ; a++){
+          if (one % a == 0){
+              return a;
+              break;
+          }
+      }
+  }else{
+      return one;
+  }
+  
+  
+}
