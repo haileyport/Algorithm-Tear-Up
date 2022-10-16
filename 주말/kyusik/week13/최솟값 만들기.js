c0653b8adb2e07f7916158a3a36962ca57241749
starplanter93 <starplanter93@gmail.com>
2022-10-16 21:51:28 +0900

    [PGS]최솟값 만들기 / level2 / 30분

---
+++ 주말/kyusik/week13/최솟값 만들기.js
@@ -0,0 +1,12 @@
+function solution(A, B) {
+    let answer = 0;
+      
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+  
+    for (let i = 0; i < A.length; i++) {
+      answer += A[i] * B[i];
+    }
+  
+    return answer;
+  }
