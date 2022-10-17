c982f9bd854554de2b2b0225c7dab2cdad72d0ee
haileyport <sugar9935@gmail.com>
2022-10-17 10:45:31 +0900

    [Manage] create 수요일

--- 주중7A/Yunhee/week11/수박수박수박수박수?.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-    let answer = "";
-    
-    for(let i = 0; i < n; i++){
-        answer = i % 2 === 0 ? answer + "수" : answer + "박"; 
-    }
-    
-    return answer;
-}
--- 주중7A/heekyung/week11/수박수박수박수박수박수?.js
+++
@@ -1,7 +0,0 @@
-function solution(n) {
-    var answer = '';
-    for (let i = 0; i < n; i++) {
-        answer += i % 2 ? '박' : '수'
-    }
-    return answer;
-}
