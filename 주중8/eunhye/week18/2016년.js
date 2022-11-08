103b3a8ec5c17924db5c3fed3cfaafac8b572070
haileyport <sugar9935@gmail.com>
2022-11-08 20:26:27 +0900

    [PGS] 2016년 / lv1 (다시)

--- 주중8/eunhye/week18/2016년.js
+++ 주중8/eunhye/week18/2016년.js
@@ -9,3 +9,18 @@ function solution(a, b) {
   }
   return weeks[daySum % 7];
 }
+
+function solution(a, b) {
+  let days = b;
+  for (i = 1; i < a; i++) {
+    if (i < 8) {
+      days += i % 2 === 0 ? 30 : 31;
+    } else {
+      days += i % 2 === 0 ? 31 : 30;
+    }
+    days -= i === 2 ? 1 : 0;
+  }
+  const weeks = ['', 'FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
+  const idx = days % 7 === 0 ? 7 : days % 7;
+  return weeks[idx];
+}
