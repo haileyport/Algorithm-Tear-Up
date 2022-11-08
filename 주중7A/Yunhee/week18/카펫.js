ba1be1343e4e109afb5dd1ecc95089a9618067e0
KimYunhee <qnvnfls@naver.com>
2022-11-08 23:00:43 +0900

    [PGS] 카펫 / Level 2 / 30분

---
+++ 주중7A/Yunhee/week18/카펫.js
@@ -0,0 +1,21 @@
+function solution(brown, yellow) {
+    var answer = [];
+    // brown + yellow === width * height와 같으므로 전체 너비
+    let sum = brown + yellow; 
+    
+    //카펫의 최소높이는 3부터(위아래 갈색, 가운데가 노란색이기 때문에)
+    for(let height = 3; height <= brown; height++){
+        //임의의 높이로 나눌때 나머지가 없을경우만
+        if(sum % height === 0){
+            //가로길이
+            let weight = sum / height;
+            
+            //테두리를 제외한 길이를 구해야하기 때문에 각각 -2해준뒤 곱셈
+            //결과가 yellow와 같다면 해당 높이와 길이 리턴
+            if( (height-2) * (weight-2) === yellow){
+                return [weight, height];
+            }
+        }
+    }
+    return answer;
+}
