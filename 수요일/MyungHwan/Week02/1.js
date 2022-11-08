ef719c816b524d6a8a3b2e4f6647d40c8f717fb1
[firstname lastname] <[valid-email]>
2022-11-09 00:05:50 +0900

    [PGS] 문자열 내 p와 y의 개수/ Level1 / 12분 16초

--- 수요일/MyungHwan/Week02/1.js
+++ 수요일/MyungHwan/Week02/1.js
@@ -7,3 +7,13 @@ function solution(s){
         return true
     } return false
 }
+
+// function numPY(s){
+//     //함수를 완성하세요
+//       return s.toUpperCase().split("P").length === s.toUpperCase().split("Y").length;
+//   }
+  
+  
+//   // 아래는 테스트로 출력해 보기 위한 코드입니다.
+//   console.log( numPY("pPoooyY") )
+//   console.log( numPY("Pyy") )
