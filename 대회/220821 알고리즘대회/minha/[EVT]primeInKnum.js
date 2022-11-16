993bb7baf5d929e613d6857537b4a15216b7ce42
haileyport <sugar9935@gmail.com>
2022-11-16 22:14:59 +0900

    conflict resolve

--- 대회/220821 알고리즘대회/minha/[EVT]primeInKnum.js
+++ 대회/220821 알고리즘대회/minha/[EVT]primeInKnum.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function solution(n, k) {
   var answer = 0;
   let kn = n.toString(k);
@@ -18,25 +17,4 @@ const isPrime = (n) => {
     }
     return (count === 0) ? true : false;
   }
-=======
-function solution(n, k) {
-  var answer = 0;
-  let kn = n.toString(k);
-  let testNum = kn.split(0);
-  testNum.map(el => {
-    if (el.length > 0 && isPrime(Number(el))) answer++;
-  })
-  return answer;
-}
-
-const isPrime = (n) => {
-  if (n === 1) return false;
-  else {
-    let count = 0;
-    for (let i = 2; i <= Math.sqrt(n); i++) {
-      if (n % i === 0) count++;
-    }
-    return (count === 0) ? true : false;
-  }
->>>>>>> 5e9358ed1d66fd797ecc2342022c15d3cc3c9ef9
 }
