ed3ea6c40a71d534e19766d9f9cd931d13946ff8
ssunip <ssunip99@gmail.com>
2022-11-30 19:17:47 +0900

    [PGS] 행렬의 덧셈 / Level 1 / 10min

--- 수요일/Insun/Week05/28. 행렬의 덧셈.js
+++ 수요일/Insun/Week05/28. 행렬의 덧셈.js
@@ -1,10 +1,4 @@
 function solution(arr1, arr2) {
   //arr1[0][0]과 arr2[0][0]을 더해야한다
-  // for(let i = 0; i < arr1.length; i++) {
-  //     for(let j = 0; j < arr1[i].length; j++) {
-  //          arr1[i][j] = arr1[i][j] + arr2[i][j]
-  //     }
-  //  }
-  //  return arr1
   return arr1.map((a, i) => a.map((b, j) => b + arr2[i][j]));
 }
